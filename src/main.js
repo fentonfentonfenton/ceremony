3b9154c3c535b5f0258438a6db8d0682fad846ff
Jack Fenton <13979377+fentonfentonfenton@users.noreply.github.com>
2024-09-05 12:27:26 +0100

    make markdown pages easy

--- src/main.js
+++ src/main.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './Home';
-import Venue from './Venue'
+import Page from './Page';
 
 const Main = () => {
     return (
         <Routes> {/* The Switch decides which component to show based on the current URL.*/}
             <Route exact path='/' element={<Home />}></Route>
-            <Route exact path='/venue' element={<Venue />}></Route>
-
+            <Route exact path='/venue' element={<Page page='venue' />}></Route>
+            <Route exact path='/faq' element={<Page page='faq' />}></Route>
+            <Route exact path='/lineup' element={<Page page='lineup' />}></Route>
             {/* <Route exact path='/rsvp' component={Rsvp}></Route> */}
         </Routes>
     );
