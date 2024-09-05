f7aa9217616ddbf1a1a26c65857359eb45fe4c66
Jack Fenton <13979377+fentonfentonfenton@users.noreply.github.com>
2024-09-05 13:49:51 +0100

    ravemode

--- src/Home.js
+++ src/Home.js
@@ -5,10 +5,7 @@ import ProTip from './ProTip';
 import { useEffect, useState } from 'react';
 
 
-export default function Home() {
-
-    const [rave, setRave] = useState(false);
-
+export default function Home({ rave }) {
 
     function RaveMode(rave) {
         return (
@@ -22,8 +19,9 @@ export default function Home() {
             <Typography variant="h5" sx={{ my: 2, mb: "10%" }}>
                 This is the one thing we didn't want to happen
             </Typography>
-            <Box sx={{ mb: "70%" }}>
+            <Box>
                 <ProTip rave={rave} />
+
                 {RaveMode(rave)}
             </Box>
         </>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 import Home from './Home';
 import Page from './Page';
 
-const Main = () => {
+const Main = ({ rave }) => {
+
     return (
         <Routes> {/* The Switch decides which component to show based on the current URL.*/}
-            <Route exact path='/' element={<Home />}></Route>
+            <Route exact path='/' element={<Home rave={rave} />}></Route>
             <Route exact path='/venue' element={<Page page='venue' />}></Route>
             <Route exact path='/faq' element={<Page page='faq' />}></Route>
             <Route exact path='/lineup' element={<Page page='lineup' />}></Route>
