44a5427df39f6aee65499eb50fcdcc31850c3f10
Jack <jack@Jacks-Mac-mini.local>
2024-09-06 06:53:24 +0100

    css on form

--- src/Rsvp.js
+++ src/Rsvp.js
@@ -1,5 +1,6 @@
 
 import * as React from 'react';
+import Container from '@mui/material/Container';
 
 export default function Rspv({ rave }) {
 
@@ -12,7 +13,9 @@ export default function Rspv({ rave }) {
 
     return (
         <>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSetsVj_5om3zB1PPVBxnezTTXP6PkQVtFIU4Dz27VR2zqKoTQ/viewform?embedded=true" width="340" height="995" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <Container height="100%" width="100" id="gForm">
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSetsVj_5om3zB1PPVBxnezTTXP6PkQVtFIU4Dz27VR2zqKoTQ/viewform?embedded=true" width="600" height="1500" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </Container>
         </>
     )
 }
